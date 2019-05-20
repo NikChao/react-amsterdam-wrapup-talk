89198f8657e35d9e298ef79bb6a43ad290b8287e
NikChao <nik.chaourov@gmail.com>
2019-05-20 16:18:20 +1000

    even better slides

--- slides/Using ESLint and Prettier in a TypeScript Project - DEV Community 👩_💻👨_💻_files/webShare-20c2d4a0b014bba9e2c0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([8],{163:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var c=t(164);t.n(c)}},[163]);
-//# sourceMappingURL=webShare-20c2d4a0b014bba9e2c0.js.map
